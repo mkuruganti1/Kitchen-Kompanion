95a9114444d800e1d589d89ad2c1c8614ce27e16
Genevieve Sampson <gsampson10@gmail.com>
2022-10-24 18:09:10 -0400

    submit and approve requests screen

---
+++ requests.js
@@ -0,0 +1,67 @@
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+
+document.addEventListener("DOMContentLoaded", getTodos);
+todoList.addEventListener("click", deleteTodo);
+
+function addTodo(e) {
+  e.preventDefault();
+  const todoDiv = document.createElement("div");
+  todoDiv.classList.add("todo");
+  const newTodo = document.createElement("li");
+  newTodo.innerText = todoInput.value;
+  newTodo.classList.add("todo-item");
+  todoDiv.appendChild(newTodo);
+  todoInput.value = "";
+  const completedButton = document.createElement("button");
+  completedButton.innerHTML = `✔️`;
+  completedButton.classList.add("complete-btn");
+  todoDiv.appendChild(completedButton);
+  const trashButton = document.createElement("button");
+  trashButton.innerHTML = `✖️`;
+  trashButton.classList.add("trash-btn");
+  todoDiv.appendChild(trashButton);
+  todoList.appendChild(todoDiv);
+}
+
+function deleteTodo(e) {
+  const item = e.target;
+
+  if (item.classList[0] === "trash-btn") {
+    const todo = item.parentElement;
+    todo.classList.add("fall");
+    todo.addEventListener("transitionend", (e) => {
+      todo.remove();
+    });
+  }
+  if (item.classList[0] === "complete-btn") {
+    const todo = item.parentElement;
+    todo.classList.add("fall_left");
+    todo.addEventListener("transitionend", (e) => {
+      todo.remove();
+    });
+  }
+}
+
+function getTodos() {
+  let todos = ['Goldfish', 'Oreos', 'Cheese Sticks'];
+
+  todos.forEach(function (todo) {
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo");
+    const newTodo = document.createElement("li");
+    newTodo.innerText = todo;
+    newTodo.classList.add("todo-item");
+    todoDiv.appendChild(newTodo);
+    const completedButton = document.createElement("button");
+    completedButton.innerHTML = `✔️`;
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton);
+    const trashButton = document.createElement("button");
+    trashButton.innerHTML = `✖️`;
+    trashButton.classList.add("trash-btn");
+    todoDiv.appendChild(trashButton);
+    todoList.appendChild(todoDiv);
+  });
+}
