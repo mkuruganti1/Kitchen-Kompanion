0c5919c8eccb702e9c2fac5be1b0bc5d7e208348
mkuruganti1 <55095015+mkuruganti1@users.noreply.github.com>
2022-10-29 20:23:51 -0400

    delete items from fridge

---
+++ fridge.js
@@ -0,0 +1,46 @@
+function removeCheckedCheckboxes() {
+  var checked = document.querySelectorAll(".delete-checkbox:checked");
+  checked.forEach((elem) => {
+    elem.parentElement.style.display = "none";
+  })
+}
+
+function addItemToFridge() {
+  var node = document.createElement()
+}
+
+// function modalPopup() {
+//   // Get the button that opens the modal
+//   const btn = document.getElementById("add-button");
+//   btn.addEventListener('click', function() {
+//     // Get the modal
+//     var modal = document.getElementById("modal");
+    
+
+//     // Get the <span> element that closes the modal
+//     var span = document.getElementsByClassName("close")[0];
+
+//     // When the user clicks on the button, open the modal
+//     btn.onclick = function() {
+//       modal.style.display = "block";
+//     }
+
+//     // When the user clicks on <span> (x), close the modal
+//     span.onclick = function() {
+//       modal.style.display = "none";
+//     }
+
+//     // When the user clicks anywhere outside of the modal, close it
+//     window.onclick = function(event) {
+//       if (event.target == modal) {
+//         modal.style.display = "none";
+//       }
+//     }
+//   });
+// }
+
+
+
+
+
+
