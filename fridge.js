ea7fcd78cad258acd3b02de84b84d9abfe2ac5cc
Genevieve Sampson <gsampson10@gmail.com>
2022-11-01 13:23:00 -0400

    save fridge items between tabs

--- fridge.js
+++ fridge.js
@@ -1,16 +1,32 @@
+document.addEventListener("DOMContentLoaded", getFridgeItems);
+
 function removeCheckedCheckboxes() {
   var checked = document.querySelectorAll(".delete-checkbox:checked");
+  var fridgeItems = JSON.parse(localStorage.getItem("fridgeItems") || "[]");
   checked.forEach((elem) => {
     elem.parentElement.parentElement.remove();
+    let item = elem.value;
+    for(let i = 0; i < fridgeItems.length; i++){
+      if(fridgeItems[i][0] == item){
+        fridgeItems.splice(i, 1);
+      }
+    }
   })
+  localStorage.setItem("fridgeItems", JSON.stringify(fridgeItems));
 }
 
 function addItemToFridge() {
+
   // get items from form
   var item = document.getElementById("itemName").value;
   var category = document.getElementById("category").options[document.getElementById("category").selectedIndex].text;
   var quantity = document.getElementById("quantity").value;
   var expirationdate = document.getElementById("expdate").value;
+
+  //save new item to localStorage 
+  var fridgeItems = JSON.parse(localStorage.getItem("fridgeItems") || "[]");
+  fridgeItems.push([item, category, quantity, expirationdate]);
+  localStorage.setItem("fridgeItems", JSON.stringify(fridgeItems));
 
   // create the input and label elements, and append the input to label
   var label = document.createElement("label");
@@ -50,6 +66,42 @@ function addItemToFridge() {
   fridge.style.display = "block";
   
   document.getElementById("myForm").reset();
+}
+
+function getFridgeItems(){
+    var fridgeItems = JSON.parse(localStorage.getItem("fridgeItems") || "[]");
+    fridgeItems.forEach(populateSingleFridgeItem);
+}
+
+function populateSingleFridgeItem(item){
+      // create the input and label elements, and append the input to label
+      var label = document.createElement("label");
+      var input = document.createElement("input");
+      input.setAttribute("type", "checkbox");
+      input.setAttribute("name", "item");
+      input.setAttribute("value", item[0]);
+      input.setAttribute("class", "delete-checkbox");
+      input.setAttribute("id", item[1]);
+      
+      label.appendChild(input);
+      label.append(item[0]);
+      
+      // create the span class for the expand img
+      var span = document.createElement("span");
+      span.setAttribute("class", "material-icons material-symbols-outlined")
+      span.setAttribute("id", "expand");
+      span.setAttribute("onclick", "showItemInfo()")
+      span.append("open_in_new");
+    
+      // create the new div that holds the label/input and span, and add label/input and span to new div
+      var newdiv = document.createElement("div")
+      newdiv.className = "items-in-fridge";
+      newdiv.appendChild(label);
+      newdiv.appendChild(span);
+      
+      // add all new elements to the list of items in fridge
+      var element = document.getElementById("fridge-items-form");
+      element.appendChild(newdiv);  
 }
 
 function formPopup() {
